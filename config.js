5116511f931992c90c6aa95c2d961da53fab7f93
pat <patrickhughes013@gmail.com>
2021-09-04 10:31:39 -0500

    Update config.js
    Trying to remove the blog link.
        {
          label: 'Blog',
          path: '/'
        },

--- config.js
+++ config.js
@@ -1,39 +1,35 @@
 module.exports = {
   url: 'https://gatsby-starter-blog-and-cv.netlify.app',
   pathPrefix: '/',
-  title: 'Blog by Marat Dospolov',
+  title: 'Blog by Patrick Hughes',
   keywords:
     'account surfer, trello bookmark, trello, productivity, javascript, Marat Dospolov, choosy windows, browser selector',
-  subtitle: 'Developer. Optimizer. Learner.',
-  copyright: '© 2020 | Kyiv, Ukraine',
-  disqusShortname: 'dospolov',
+  subtitle: 'Developer. Innovator. Learner.',
+  copyright: '© 2021 | BMD HODL',
+  disqusShortname: 'pat',
   postsPerPage: 20,
   googleAnalyticsId: 'UA-6589522-7',
   menu: [
-    {
-      label: 'Blog',
-      path: '/'
-    },
     {
       label: 'CV',
       path: '/cv'
     }
   ],
   author: {
-    name: 'Marat Dospolov',
+    name: 'Patrick Hughes',
     photo: '/photo.jpg',
     bio: 'Developer. Optimizer. Learner.',
     contacts: {
       // don't remove fields, just make them empty string ''
       // https://github.com/gatsbyjs/gatsby/issues/2392
-      github: 'dospolov',
-      twitter: 'marat_dospolov',
-      linkedin: 'dospolov',
-      telegram: 'dospolov',
-      instagram: 'dospolov',
-      facebook: 'dospolov',
-      email: '',
-      rss: '/rss.xml'
+      github: 'bmdhodl',
+      twitter: 'phughes9000',
+      linkedin: 'patrickhughes013',
+      telegram: 'pat90000',
+      instagram: 'pahu9000',
+      facebook: 'patrick.hughes.98',
+      email: 'patrick.hughes013@gmail.com',
+      rss: ''
     }
   }
 }
